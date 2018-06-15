fa43dd865a4758337efd23fb5a9b83a5b539f44d
avkudr <argon24@mail.ru>
2018-06-16 00:14:57 +0200

    Start writing tests :)

---
+++ test/test_main.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('Main', () => {
+    it('should start empty', () => {
+        // Test implementation goes here
+        assert.equal(1, 0);
+    }); 
+});
